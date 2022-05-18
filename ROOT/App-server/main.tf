75b928038648fd8d3e460d3dec4d28c51cc42fe8
vida <vwienibe@ziyotek.net>
2022-05-18 18:53:52 -0400

    Adding subnet id as a variable

--- ROOT/App-server/main.tf
+++ ROOT/App-server/main.tf
@@ -3,7 +3,7 @@ module "vpc" {
 }
 
 module "app-vpc" {
-  source = "../../CHILD/"
+  source = "../../CHILD/VPC"
   vpc_cidr = "20.0.0.0/16"
   pub_cidr = "20.0.1.0/24"
   prv_cidr = "20.0.2.0/24"
@@ -12,5 +12,5 @@ module "app-vpc" {
 
 module "app-server" {
   source = "git::https://github.com/Saviour30/terraform-module.git//CHILD/EC2"
-  #source = "git@github.com/Saviour30/terraform-module//CHILD/EC2"
+
 }
